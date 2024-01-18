ad14dc8a82dbe0771a407ac8215a7cdf0529fa8b
AtajanMB <atashmammedow20@gmail.com>
2024-01-18 16:39:35 +0500

    added book

---
+++ app/src/main/java/com/example/mybook/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.mybook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.barteksc.pdfviewer.PDFView
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle
+
+class MainActivity : AppCompatActivity() {
+    lateinit var pdfView: PDFView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        pdfView = findViewById(R.id.idPDFView)
+
+        pdfView.fromAsset("halallyk_kyssalary.pdf")
+            .scrollHandle(DefaultScrollHandle(this))
+            .load()
+
+    }
+}
